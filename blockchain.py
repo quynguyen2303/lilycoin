776ca7a64ba75ff9a9132b3cec857d66c2ff4355
quynguyen2303 <nguyenphuquy2303@gmail.com>
2020-12-13 13:58:05 +0700

    4th commit

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from datetime import datetime
 from hashlib import sha256
@@ -10,15 +11,16 @@ class Blockchain(object):
 
         # Create the genesis block
         print("Creating genesis block")
-        self.new_block()
+        self.chain.append(self.new_block())
 
-    def new_block(self, previous_hash=None):
+    def new_block(self):
         # Generates a new block and adds it to the chain
         block = {
             'index': len(self.chain),
             'timestamp': datetime.utcnow().isoformat(),
             'transactions': self.pending_transactions,
-            'previous_hash': previous_hash,
+            'previous_hash': self.last_block["hash"] if self.last_block else None,
+            'nonce': format(random.getrandbits(64), "x"),
         }
         # Get the hash of this new block, and add it to the block
         block_hash = self.hash(block)
@@ -26,10 +28,6 @@ class Blockchain(object):
 
         # Reset the list of pending transactions
         self.pending_transactions = []
-        # Add the block to the chain
-        self.chain.append(block)
-
-        print(f"Created block {block['index']}")
         return block
 
 
@@ -39,12 +37,24 @@ class Blockchain(object):
         # We ensure the dictionary is sorted or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return sha256(block_string).hexdigest()
-    
+
+    @property
     def last_block(self):
         # Gets the latest block in the chain
         return self.chain[-1] if self.chain else None
 
+    def proof_of_work(self):
+        while True:
+            new_block = self.new_block()
+            if self.valid_block(new_block):
+                break
+        
+        self.chain.append(new_block)
+        print("Found a new block: ", new_block)
 
+    @staticmethod
+    def valid_block(block):
+        return block['hash'].startswith("0000")
 
     def new_transaction(self, sender, recipient, amount):
         # Adds a new transaction to the list of pending transactions
